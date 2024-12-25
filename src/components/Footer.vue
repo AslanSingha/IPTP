6711fba0b0b592389f323a0fda6740bd3d34910f
RIN SINGH <singhrin69@gmail.com>
2024-12-25 21:07:53 +0700

    initial

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+</template>
+
+<style scoped>
+
+footer{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid rgb(0, 0, 0);
+}
+
+
+</style>
+  
---
+++ src/views/PageView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page">
+    
+    <Sidebar :page="pageName" />
+    <h1>Welcome to {{ pageName }}</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar.vue';
+
+export default {
+  components: {
+    Sidebar,
+  },
+  computed: {
+    pageName() {
+      return this.$route.params.page; // Get current page from the route
+    },
+  },
+};
+</script>
+
+<style scoped>
+.page {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-left: 2px solid rgb(0, 0, 0);
+  border-right: 2px solid rgb(0, 0, 0);
+  padding: 20px;
+}
+
+</style>
